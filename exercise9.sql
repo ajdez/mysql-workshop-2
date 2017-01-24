669455df86ce871534d3a4884a97c3a5eadce1a1
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-24 21:43:33 +0000

    COmpleted Exercise 9

---
+++ exercise9.sql
@@ -0,0 +1,12 @@
+-- 1000 items in Accounts
+
+SELECT COUNT(id)
+    FROM Account;
+
+--  1100    items in Address Book
+SELECT COUNT(id)
+    FROM AddressBook;
+
+-- 4000 items in Entry
+SELECT COUNT(id)
+    FROM Entry;
