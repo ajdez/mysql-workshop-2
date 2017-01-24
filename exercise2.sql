d3105fe6ca76f8c52ec2200efd1c1d0406b9b84e
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-24 20:15:06 +0000

    Completed exercise 1 and 2

---
+++ exercise2.sql
@@ -0,0 +1,5 @@
+INSERT INTO Account VALUE (null, 'anthony.dez@gmail.com', 'baba', '2017-01-17', null);
+
+INSERT INTO AddressBook VALUE (null, 1234, 'Anthony Desormeau', '2017-01-17', null);
+
+INSERT INTO Entry VALUE (null, 1, 'Anthony', 'Desormeau', '1992-04-09', 'work', 'phone', 'Work Phone : 555-5555', null, null, null, null);
