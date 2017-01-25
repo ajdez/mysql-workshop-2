1f080625a5d64571594db61b7d2508ddf057a12a
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-25 00:11:07 +0000

    Completed Exercise 2 in Workshop 3

--- exercise13.sql
+++ exercise13.sql
@@ -1,1 +1,77 @@
 -- EXERCISE 2 from workshop 3
+
+SELECT count(id)
+    FROM Entry
+    WHERE contentLineOne like "(3%";
+    AND subtype = "phone";
+    
+SELECT count(id)
+    FROM Entry
+    WHERE type = "home"
+    AND contentLineOne not like "%5%";
+    
+SELECT count(id)
+    FROM Entry
+    WHERE type = 'work'
+    AND subtype = 'phone'
+    AND contentLineOne like "1-%";
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
